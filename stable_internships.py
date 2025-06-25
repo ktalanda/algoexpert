4f80e30cb282f8e2095beff283cd9da22b75896a
Kamil Talanda <kamil0916@gmail.com>
2025-06-25 07:18:40 +0200

    Stable internship

---
+++ stable_internships.py
@@ -0,0 +1,36 @@
+def stableInternships(interns, teams):
+    chosenInterns = {}
+    freeInterns = list(range(len(interns)))
+    currentInternChoices = [0] * len(interns)
+
+    teamMaps = []
+    for team in teams:
+        rank = {}
+        for i, internNum in enumerate(team):
+            rank[internNum] = i
+        teamMaps.append(rank)
+
+    while len(freeInterns) > 0:
+        internNum = freeInterns.pop()
+
+        intern = interns[internNum]
+        teamPreference = intern[currentInternChoices[internNum]]
+        currentInternChoices[internNum] += 1
+
+        if teamPreference not in chosenInterns:
+            chosenInterns[teamPreference] = internNum
+            continue
+
+        previousIntern = chosenInterns[teamPreference]
+        previousInternRank = teamMaps[teamPreference][previousIntern]
+        currentInternRank = teamMaps[teamPreference][internNum]
+
+        if currentInternRank < previousInternRank:
+            freeInterns.append(previousIntern)
+            chosenInterns[teamPreference] = internNum
+        else:
+            freeInterns.append(internNum)
+
+    matches = [[internNum, teamNum] for teamNum, internNum in chosenInterns.items()]
+    return matches
+        
